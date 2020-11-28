a92cb0a90c86efb35522a5fe3ec82ac289deb66e
David Rendell <David.Rendell@Softwire.com>
2020-11-28 19:20:41 +0000

    Add Cover section

---
+++ src/content/combat/cover/index.ts
@@ -0,0 +1,7 @@
+import coverContent from './cover.md'
+
+const cover = [
+  {title: "Cover", content: coverContent, breadcrumbs: ["Combat"]}
+]
+
+export default cover
--- src/content/combat/index.ts
+++ src/content/combat/index.ts
@@ -2,10 +2,12 @@ import theOrderOfCombat from './the-order-of-combat'
 import movementAndPosition from './movement-and-position'
 import actions from './actions'
 import makingAnAttack from './making-an-attack'
+import cover from "./cover"
 
 export default [
   ...theOrderOfCombat,
   ...movementAndPosition,
   ...actions,
-  ...makingAnAttack
+  ...makingAnAttack,
+  ...cover
 ]
