b386f44aac3a96acdaf897b9f3fa75e72732fa6f
David Rendell <David.Rendell@Softwire.com>
2020-11-30 13:30:57 +0000

    Add Phase Banner

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
+import PhaseBanner from "./PhaseBanner"
 import SearchArea from "./SearchArea"
 
 export default function() {
   return (
     <div>
+      <PhaseBanner />
       <h1>Keen Mind</h1>
       <SearchArea />
     </div>
---
+++ src/components/PhaseBanner.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export default function PhaseBanner() {
+  return (
+    <div className="phase-banner">
+      🧪 This site is currently in Alpha. Expect lots of bugs and missing features!
+    </div>
+  )
+}
