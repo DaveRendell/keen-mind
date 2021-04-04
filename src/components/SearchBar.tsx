87cb48aba715f61249cbb22b761e476a56e3a4d6
Sam Collard <github@swifteagle.co.uk>
2021-04-04 22:36:17 +0100

    Add shortcut to focus search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,10 +6,35 @@ interface SearchBarProps {
 }
 
 export default function ({ query, setQuery }: SearchBarProps) {
+  const inputRef = React.useRef<HTMLInputElement | null>()
+  React.useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      // Normal handling for other keys, or if we're typing / into the bar when
+      // it's already selected.
+      if (e.key !== "/" || inputRef.current === document.activeElement) {
+        return
+      }
+
+      inputRef.current?.focus()
+      e.preventDefault()
+    }
+
+    document.addEventListener("keypress", handler)
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
+
   return (
     <div className="search-bar">
       <div className="sepia">🔍</div>
-      <input value={query} onChange={e => setQuery(e.target.value)} autoFocus />
+      <input
+        ref={inputRef}
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        autoFocus
+      />
+      <div className="translucent key">/</div>
     </div>
   )
 }
